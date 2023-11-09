248c882bb879afe2d391054d9631d7b66f074a73
LAPTOP-ABJTMBMV\ismae <ied884@alumnos.unican.es>
2023-11-09 10:37:29 +0100

    icons and options

--- src/main/ventanaPrincipal.java
+++ src/main/ventanaPrincipal.java
@@ -20,6 +20,11 @@ import javax.swing.JScrollPane;
 import javax.swing.KeyStroke;
 import java.awt.event.KeyEvent;
 import java.awt.event.InputEvent;
+import java.awt.CardLayout;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 
 public class ventanaPrincipal {
 
@@ -67,23 +72,63 @@ public class ventanaPrincipal {
 		Archivo.setMnemonic('a');
 		menu.add(Archivo);
 		
+		JMenuItem ANuevo = new JMenuItem("Nuevo");
+		ANuevo.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, InputEvent.CTRL_DOWN_MASK));
+		Archivo.add(ANuevo);
+		
 		JMenuItem AAbrir = new JMenuItem("Abrir...");
-		AAbrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+		AAbrir.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK));
 		Archivo.add(AAbrir);
+		
+		JSeparator separator_1 = new JSeparator();
+		Archivo.add(separator_1);
+		
+		JMenuItem AGuardar = new JMenuItem("Guardar");
+		AGuardar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, InputEvent.CTRL_DOWN_MASK));
+		Archivo.add(AGuardar);
+		
+		JMenuItem AGuardarComo = new JMenuItem("Guardar como...");
+		Archivo.add(AGuardarComo);
 		
 		JSeparator separator = new JSeparator();
 		Archivo.add(separator);
 		
+		JMenuItem Cerrar = new JMenuItem("Cerrar");
+		Archivo.add(Cerrar);
+		
 		JMenu Edicion = new JMenu("Edicion");
 		Edicion.setMnemonic('e');
-		Edicion.setHorizontalAlignment(SwingConstants.RIGHT);
+		Edicion.setHorizontalAlignment(SwingConstants.LEFT);
 		menu.add(Edicion);
+		
+		JMenuItem ECopiar = new JMenuItem("Copiar");
+		ECopiar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, InputEvent.CTRL_DOWN_MASK));
+		Edicion.add(ECopiar);
+		
+		JMenuItem EPegar = new JMenuItem("Pegar");
+		EPegar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_V, InputEvent.CTRL_DOWN_MASK));
+		Edicion.add(EPegar);
+		
+		JMenuItem ECortar = new JMenuItem("Cortar");
+		ECortar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, InputEvent.CTRL_DOWN_MASK));
+		Edicion.add(ECortar);
+		
+		JMenuItem EBuscarReemplazar = new JMenuItem("Buscar y Reemplazar");
+		EBuscarReemplazar.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_F, InputEvent.CTRL_DOWN_MASK));
+		Edicion.add(EBuscarReemplazar);
 		
 		JToolBar herramientas = new JToolBar();
 		superior.add(herramientas);
 		
-		JButton boton = new JButton("boton");
+		JButton boton = new JButton("");
+		boton.setIcon(new ImageIcon(ventanaPrincipal.class.getResource("/media/icons8-alinear-al-centro-32.png")));
+		boton.setToolTipText("Alinear centro");
 		herramientas.add(boton);
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setToolTipText("Alinear izquierda");
+		btnNewButton.setIcon(new ImageIcon(ventanaPrincipal.class.getResource("/media/icons8-alinear-a-la-izquierda-24.png")));
+		herramientas.add(btnNewButton);
 		
 		JScrollPane center = new JScrollPane();
 		frame.getContentPane().add(center, BorderLayout.CENTER);
