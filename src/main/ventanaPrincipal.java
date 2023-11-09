9666d7fe93123960e20da0544fe0c3e8fffb7c1e
LAPTOP-ABJTMBMV\ismae <ied884@alumnos.unican.es>
2023-11-09 09:37:04 +0100

    structure

--- src/main/ventanaPrincipal.java
+++ src/main/ventanaPrincipal.java
@@ -3,6 +3,20 @@ package main;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JToolBar;
+import java.awt.GridLayout;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.SwingConstants;
+import javax.swing.JMenuItem;
+import javax.swing.JButton;
+import java.awt.FlowLayout;
+import javax.swing.JSeparator;
+import javax.swing.JTextPane;
+import javax.swing.DropMode;
+import javax.swing.JScrollPane;
 
 public class ventanaPrincipal {
 
@@ -38,6 +52,38 @@ public class ventanaPrincipal {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel superior = new JPanel();
+		frame.getContentPane().add(superior, BorderLayout.NORTH);
+		superior.setLayout(new GridLayout(2, 1, 0, 0));
+		
+		JMenuBar menu = new JMenuBar();
+		superior.add(menu);
+		
+		JMenu Archivo = new JMenu("Archivo");
+		menu.add(Archivo);
+		
+		JMenuItem AAbrir = new JMenuItem("Abrir...");
+		Archivo.add(AAbrir);
+		
+		JSeparator separator = new JSeparator();
+		Archivo.add(separator);
+		
+		JMenu Edicion = new JMenu("Edicion");
+		Edicion.setHorizontalAlignment(SwingConstants.RIGHT);
+		menu.add(Edicion);
+		
+		JToolBar herramientas = new JToolBar();
+		superior.add(herramientas);
+		
+		JButton boton = new JButton("boton");
+		herramientas.add(boton);
+		
+		JScrollPane center = new JScrollPane();
+		frame.getContentPane().add(center, BorderLayout.CENTER);
+		
+		JTextPane texto = new JTextPane();
+		center.setViewportView(texto);
 	}
 
 }
